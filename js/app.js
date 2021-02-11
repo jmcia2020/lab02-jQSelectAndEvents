d7bdba9e106e83421a6e5dacdd3580085796dcbb
jmcia2020 <jeanmmcmahon@gmail.com>
2021-02-10 22:32:41 -0600

    psuedo solutions added to js and html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 'use strict';
 const creatures = [];
-function Creature (image_url, title, description, keyword, horns){
+function Creature(image_url, title, description, keyword, horns) {
   this.image_url = image_url;
   this.title = title;
   this.description = description;
@@ -9,85 +9,71 @@ function Creature (image_url, title, description, keyword, horns){
   this.horns = horns;
 }
 
-// Creature.prototype.render = function(){
-//   const template = $('#photo-template').clone().contents();
-//   template.attr('id', 'photo-template-new');
-//   template.attr('class', this.keyword);
-//   const h2 =template.find('h2');
-//   const image_url = template.find('img');
-//   const p = template.find('p');
-// };
 
-Creature.prototype.render = function(){
+Creature.prototype.render = function () {
   const photoTemplate = document.getElementById('photo-template').innerHTML;
-  const renderedHTML = Mustache.render(photoTemplate,{title: this.title, image_url: this.image_url, description: this.description, keyword: this.keyword, horns: this.horns}) 
+  const renderedHTML = Mustache.render(photoTemplate, this);
   $('main').append(renderedHTML);
 
   console.log(renderedHTML);
 };
 
-// const template = $('#photo-template').clone().contents();
-// template.attr('id', 'photo-template-new');
-// template.attr('class', this.keyword);
-// const h2 =template.find('h2');
-// const image_url = template.find('img');
-// const p = template.find('p');
-
-// //Populate the elements
-// h2.text(this.title);
-// image_url.attr('src', this.image_url);
-// p.text(this.description);
-
-// Add to the page
-// $('main').append(template);
-
-
 //json part of demo
 
 //retrieving data from an outside source USE for lab
+// put in function to empty array and and reload data from page-2json
+// create 2 buttons to clear HTML & page and load in dataSet2 
 
-$.ajax('/data/page-1.json').then(creaturesJSON => {
-  creaturesJSON.forEach(properties => {
-    const creature = new Creature(
-      properties.image_url,
-      properties.title,
-      properties.description,
-      properties.keyword,
-      properties.horns);
-    addSelect(creature.keyword);
-    creatures.push(creature);
+function loadCreatures(dataSet) {
+  // clearCreatureData();  TO CREATE!!!!
+    // eventlistener onclick <div class="process"><button onclick="process()">Process Template1</button></div>
+    // eventlistener onclick <div class="process"><button onclick="process()">Process Template2</button></div>
+
+  $.ajax(dataSet).then(creaturesJSON => {
+    creaturesJSON.forEach(properties => {
+      const creature = new Creature(
+        properties.image_url,
+        properties.title,
+        properties.description,
+        properties.keyword,
+        properties.horns);
+      addSelect(creature.keyword);
+      creatures.push(creature);
+    });
+
+    // clearRenderedHTML(); TO CREATE!!!!
+
+    for (const creature of creatures) {
+      creature.render();
+    }
   });
-  for (const creature of creatures) {
-    creature.render();
-  }
-});
-
+}
+loadCreatures('/data/page-1.json');
 
 function addSelect(filter) {
-  const selectElement = $('select');  
+  const selectElement = $('select');
   const option = $('<option></option>').text(filter);
-  let existance=false;
-  selectElement.children().each(function(){
-    if (this.text === filter){
+  let existance = false;
+  selectElement.children().each(function () {
+    if (this.text === filter) {
       existance === true;
-    }  
+    }
   });
-  if (existance){
+  if (existance) {
     return;
   }
   selectElement.append(option);
 }
 
-document.getElementById('keywordSelect').addEventListener('change',updatePage);
-
-function updatePage(){
+document.getElementById('keywordSelect').addEventListener('change', updatePage);
+function updatePage() {
   const selectedKeyword = $('#keywordSelect option:selected').text();
-  var elements = $('[id="photo-template-new"]');
-  for( const element of elements){
-    if ( $(element).hasClass(selectedKeyword)){
+  var elements = $('.dataSection');
+  for (const element of elements) {
+    if ($(element).hasClass(selectedKeyword)) {
       $(element).show();
     }
-    else{
+    else {
       $(element).hide();
     }
   }
